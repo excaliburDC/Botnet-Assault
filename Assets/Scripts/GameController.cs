cd8078ddc8d339a6ca9bc6844007b2197e235ee7
DC <43692686+excaliburDC@users.noreply.github.com>
2020-11-04 12:49:40 +0530

    basic scene setup

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject server;
+
+    public List<GameObject> clients;
+
+    private List<LineRenderer> lines = new List<LineRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject gObj in clients)
+        {
+            LineRenderer lr = gObj.GetComponent<LineRenderer>();
+
+            lines.Add(lr);
+
+            SetLinePostions(lr, gObj);
+        }
+    }
+
+    private void SetLinePostions(LineRenderer lr, GameObject gObj)
+    {
+        lr.SetPosition(0, gObj.transform.position);
+        lr.SetPosition(1, server.transform.position);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PoolObj
+{
+    public string poolName;
+    public GameObject poolPrefab;
+    public int size;
+}
+
+public class PoolManager : SingletonManager<PoolManager>
+{
+    public List<PoolObj> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    private void Awake()
+    {
+       // DontDestroyOnLoad(this.gameObject);
+
+    }
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (PoolObj pool in pools)
+        {
+            Queue<GameObject> objectToPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject gObj = Instantiate(pool.poolPrefab);
+                gObj.SetActive(false);
+                gObj.transform.parent = this.transform;
+                objectToPool.Enqueue(gObj);
+
+            }
+
+            poolDictionary.Add(pool.poolName, objectToPool);
+        }
+
+    }
+
+    public GameObject SpawnInWorld(string tag,Vector3 position,Quaternion rotation)
+    {
+        if(!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogError("Object with tag " + tag + " not found...");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
